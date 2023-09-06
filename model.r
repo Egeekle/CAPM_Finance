7d113141992e57f859b2a92b2d53ef096a963206
Eddisoon <edison.velazco@unmsm.edu.pe>
2023-09-06 03:42:21 -0500

    primer repo

---
+++ model.r
@@ -0,0 +1,18 @@
+##create a random forest model in R  
+install.packages("languageserver")
+install.packages("tidyverse")
+install.packages("caret")
+
+install.packages("readxl")
+
+
+
+#create a clasifier model in R 
+
+
+library(tidyverse)
+
+#create a clasifier model in R 
+library(caret)
+
+
